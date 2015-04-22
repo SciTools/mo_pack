23eb8f1d0429a6d9b12a8e0807fa1d82bfb5faf4
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2015-04-22 17:10:01 -0400

    Add PyPI classifiers.

--- setup.py
+++ setup.py
@@ -21,4 +21,21 @@ setup(
     ext_modules=cythonize(extensions),
     packages=['mo_pack'],
     package_dir={'': 'lib'},
+    classifiers=[
+            'Development Status :: 3 - Alpha',
+            ('License :: OSI Approved :: '
+             'GNU Lesser General Public License v3 or later (LGPLv3+)'),
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: POSIX',
+            'Operating System :: POSIX :: AIX',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: GIS',
+    ],
 )
